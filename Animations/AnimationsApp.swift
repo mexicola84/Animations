bb84ccb4710f4378facaca5ab48b9750999f3381
mexicola84 <i@jan-grimm.com>
2023-12-07 14:58:07 +0100

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Jan Grimm on 07.12.23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

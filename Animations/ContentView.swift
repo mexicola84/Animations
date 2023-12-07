72e41c09db0a781758483e9e1066d9367a0eef22
mexicola84 <i@jan-grimm.com>
2023-12-07 15:31:54 +0100

    Day 32 doing implicit animation in the technical Animations project. learnt about different animation types and their modifiers as well as repeating animations.

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 1.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        // day 32 implicit animations
+        Button("Tap me") {
+            //animationAmount += 1
         }
-        .padding()
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        //customizing implicit animations.
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(.easeOut(duration: 1).repeatForever(autoreverses: false), value: animationAmount) //value needed as the animation may start always when a view is changed, like flipping to phone to horizontal view.
+        )
+        .onAppear{
+            animationAmount = 2
+        }
     }
 }
 

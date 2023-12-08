5eecae3d86af809997830b8923e111430b7fcf96
mexicola84 <i@jan-grimm.com>
2023-12-08 09:24:43 +0100

    Day 33 of 100 Days of SwiftUI, learnt about controlling the animation stack with implicit and explicit animations. Also learnt about dragging text and stacks around the screen and controlling animation with CGSize as well as DragGestures.

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -10,63 +10,113 @@ import SwiftUI
 struct ContentView: View {
 //    @State private var animationAmount = 1.0 //impicit animations
 //    @State private var animationAmount = 2.0 //implicit binding animations
-    @State private var animationAmount = 0.0 // explicit animations
+//    @State private var animationAmount = 0.0 // explicit animations
+//    @State private var enabled = false
+    let letters = Array("Hello SwiftUI") // animating gestures
+    
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero // animating gestures
     
     var body: some View {
-//        // day 32 implicit animations
-//        Button("Tap me") {
-//            //animationAmount += 1
-//        }
-//        .padding(50)
-//        .background(.red)
-//        .foregroundStyle(.white)
-//        .clipShape(.circle)
-//        //customizing implicit animations.
-//        .overlay(
-//            Circle()
-//                .stroke(.red)
-//                .scaleEffect(animationAmount)
-//                .opacity(2 - animationAmount)
-//                .animation(.easeOut(duration: 1).repeatForever(autoreverses: false), value: animationAmount) //value needed as the animation may start always when a view is changed, like flipping to phone to horizontal view.
-//        )
-//        .onAppear{
-//            animationAmount = 2
+        //        // day 32 implicit animations
+        //        Button("Tap me") {
+        //            //animationAmount += 1
+        //        }
+        //        .padding(50)
+        //        .background(.red)
+        //        .foregroundStyle(.white)
+        //        .clipShape(.circle)
+        //        //customizing implicit animations.
+        //        .overlay(
+        //            Circle()
+        //                .stroke(.red)
+        //                .scaleEffect(animationAmount)
+        //                .opacity(2 - animationAmount)
+        //                .animation(.easeOut(duration: 1).repeatForever(autoreverses: false), value: animationAmount) //value needed as the animation may start always when a view is changed, like flipping to phone to horizontal view.
+        //        )
+        //        .onAppear{
+        //            animationAmount = 2
         
-//        }
+        //        }
         
         //biding animation starts here
-//        print(animationAmount)
-//
-//        return VStack {
-//            Stepper("Scale amount", value: $animationAmount.animation(
-//                .easeInOut(duration: 1)
-//                .repeatCount(3, autoreverses: true)
-//            ), in: 1...10)
-//
-//            Spacer()
-//
-//            Button("Tap me") {
-//                animationAmount += 1
-//            }
-//            .padding(40)
-//            .background(.red)
-//            .foregroundStyle(.white)
-//            .clipShape(.circle)
-//            .scaleEffect(animationAmount)
-//        }
+        //        print(animationAmount)
+        //
+        //        return VStack {
+        //            Stepper("Scale amount", value: $animationAmount.animation(
+        //                .easeInOut(duration: 1)
+        //                .repeatCount(3, autoreverses: true)
+        //            ), in: 1...10)
+        //
+        //            Spacer()
+        //
+        //            Button("Tap me") {
+        //                animationAmount += 1
+        //            }
+        //            .padding(40)
+        //            .background(.red)
+        //            .foregroundStyle(.white)
+        //            .clipShape(.circle)
+        //            .scaleEffect(animationAmount)
+        //        }
         
         //explicit animations starts here
-        Button("Tap Me") {
-            withAnimation(.spring(duration: 1, bounce: 0.5)) {
-                animationAmount += 360
+        //        Button("Tap Me") {
+        //            withAnimation(.spring(duration: 1, bounce: 0.5)) {
+        //                animationAmount += 360
+        //            }
+        //        }
+        //        .padding(50)
+        //        .background(.red)
+        //        .foregroundStyle(.white)
+        //        .clipShape(.circle)
+        //        .rotation3DEffect(
+        //            .degrees(animationAmount), axis: (x: 0.5, y: 1, z: 1)
+        //        )
+        
+        //day 33 starts here; controlling the animation stack
+        //        Button("Tap Me") {
+        //            enabled.toggle()
+        //        }
+        //        .frame(width: 200, height: 200)
+        //        .background(enabled ? .red : .blue)
+        //        .foregroundStyle(.white)
+        //        .animation(.default, value: enabled)
+        //        .clipShape(.rect(cornerRadius: enabled ? 60 : 30))
+        //        .animation(.spring(duration: 1, bounce: 0.6), value: enabled)
+        //
+        //animation gestures
+        //        LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: //.bottomTrailing)
+        //            .frame(width: 300, height: 200)
+        //            .clipShape(.rect(cornerRadius: 10))
+        //            .offset(dragAmount)
+        //            .gesture(
+        //                DragGesture()
+        //                    .onChanged { dragAmount = $0.translation }
+        //                    .onEnded { _ in
+        //                        withAnimation(.bouncy) { //explicit animation
+        //                            dragAmount = .zero
+        //                        }
+        //                    }
+        //            )
+        //            .animation(.bouncy, value: dragAmount) //implicit animation
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count, id: \.self) { num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(.linear.delay(Double(num) / 20), value: dragAmount)
             }
         }
-        .padding(50)
-        .background(.red)
-        .foregroundStyle(.white)
-        .clipShape(.circle)
-        .rotation3DEffect(
-            .degrees(animationAmount), axis: (x: 0.5, y: 1, z: 1)
+        .gesture(
+            DragGesture()
+                .onChanged { dragAmount = $0.translation }
+                .onEnded { _ in
+                        dragAmount = .zero
+                    enabled.toggle()
+                }
         )
     }
 }
